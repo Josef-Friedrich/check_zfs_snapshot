6eb98daa48c8f971a4eb16c34ce2038a42b3fc70
Josef Friedrich <josef@friedrich.rocks>
2022-11-20 20:49:33 +0100

    Configure the argparser

--- check_zfs_snapshot.py
+++ check_zfs_snapshot.py
@@ -2,25 +2,59 @@
 
 import argparse
 from argparse import ArgumentParser
-import nagiosplugin
+from nagiosplugin.runtime import guarded
+from typing import cast
+
 
 class OptionContainer:
     pass
 
+
 opts: OptionContainer = OptionContainer()
+
 
 def get_argparser() -> ArgumentParser:
     parser: ArgumentParser = argparse.ArgumentParser(
-        prog="check_zpool_scrub",  # To get the right command name in the README.
+        prog="check_zfs_snapshot",  # To get the right command name in the README.
     )
+
+    parser.add_argument(
+        "-c",
+        "--critical",
+        help="Interval in seconds for critical state.",
+    )
+
+    parser.add_argument(
+        "-d",
+        "--dataset",
+        help="The ZFS dataset to check.",
+    )
+    parser.add_argument(
+        "-s",
+        "--short-description",
+        help="Show a short description of the command.",
+    )
+    parser.add_argument(
+        "-v",
+        "--version",
+        help="Show the version number.",
+    )
+
+    parser.add_argument(
+        "-w",
+        "--warning",
+        help="Interval in seconds for warning state. Must be lower than -c",
+    )
+
     return parser
 
 
-@nagiosplugin.guarded(verbose=0)
+# @guarded(verbose=0)
 def main():
     pass
     global opts
-    opts = get_argparser().parse_args()
+    opts = cast(OptionContainer, get_argparser().parse_args())
+
 
 if __name__ == "__main__":
     main()
